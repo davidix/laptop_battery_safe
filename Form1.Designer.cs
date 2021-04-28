ce7c47a5d92105f3488caf7e4161487b42045144
davood bereihi <d.bereihi@gmail.com>
2021-04-28 13:25:26 +0430

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace laptop_battery_safe
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CheckerTimer = new System.Windows.Forms.Timer(this.components);
+            this.Restarter = new System.Windows.Forms.Timer(this.components);
+            this.lblBatteryPercentage = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnSetLimit = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CheckerTimer
+            // 
+            this.CheckerTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Restarter
+            // 
+            this.Restarter.Tick += new System.EventHandler(this.Restarter_Tick);
+            // 
+            // lblBatteryPercentage
+            // 
+            this.lblBatteryPercentage.AutoSize = true;
+            this.lblBatteryPercentage.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblBatteryPercentage.Location = new System.Drawing.Point(186, 84);
+            this.lblBatteryPercentage.Name = "lblBatteryPercentage";
+            this.lblBatteryPercentage.Size = new System.Drawing.Size(94, 40);
+            this.lblBatteryPercentage.TabIndex = 0;
+            this.lblBatteryPercentage.Text = "label1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSetLimit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnSetLimit
+            // 
+            this.btnSetLimit.Name = "btnSetLimit";
+            this.btnSetLimit.Size = new System.Drawing.Size(65, 20);
+            this.btnSetLimit.Text = "Set Limit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 133);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblBatteryPercentage);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Davidix Battery Safe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer CheckerTimer;
+        private System.Windows.Forms.Timer Restarter;
+        private System.Windows.Forms.Label lblBatteryPercentage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnSetLimit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+
+namespace laptop_battery_safe
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            Application.Idle += Application_Idle;
+        }
+        int limit = 95;
+
+        PowerStatus power = SystemInformation.PowerStatus;
+
+       
+        private void Application_Idle(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            CheckerTimer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblBatteryPercentage.Text = (power.BatteryLifePercent * 100).ToString();
+            if (power.PowerLineStatus == PowerLineStatus.Online && power.BatteryLifePercent * 100 > limit)
+            {
+                msg();
+            }
+        }
+        public void msg()
+        {
+            CheckerTimer.Stop();
+            for (int i = 0; i < 5; i++)
+                Console.Beep(1500, 100);
+            for (int j = 0; j < 5; j++)
+                Console.Beep(3000, 80);
+            for (int k = 0; k < 5; k++)
+                Console.Beep(500, 80);
+
+            MessageBox.Show("Battery full;");
+            Restarter.Start();
+            Restarter.Interval = (60 * 1 * 500);
+        }
+
+        private void Restarter_Tick(object sender, EventArgs e)
+        {
+            CheckerTimer.Start();
+            Restarter.Stop();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            limit = Convert.ToInt32(textBox1.Text);
+        }
+    }
+}
